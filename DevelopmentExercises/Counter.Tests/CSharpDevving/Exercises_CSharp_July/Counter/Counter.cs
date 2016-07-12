65cb3676356d86d2dd8092460c48c99704183aea
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-12 13:06:18 +0100

    Added Test headers and new solution.

---
+++ DevelopmentExercises/Counter.Tests/CSharpDevving/Exercises_CSharp_July/Counter/Counter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    public class Exercises
+    {
+        public static void Main()
+        {
+        }
+    }
+
+    public class Counter
+    {
+        public static void Request()
+        {
+        }
+
+        public static void NumberOfTimesCalled(string callCount)
+        {
+        }
+    }
+
+   
+}
---
+++ DevelopmentExercises/Counter.Tests/CSharpDevving/Exercises_CSharp_July/Counter/CounterTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Counter
+{
+   [TestFixture]
+    public class CounterTests
+    {
+       [Test]
+       public static void RequestChecker()
+       {
+       }
+
+    }
+}
