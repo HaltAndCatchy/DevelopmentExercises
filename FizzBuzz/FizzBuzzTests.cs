94e8f8ef9adccc57191e75c5e54ab69388e299e4
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-13 17:18:27 +0100

    Create FizzBuzz project.

---
+++ FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public static void ReturnsString()
+        {
+            string sayOutPut = FizzBuzz.Say();
+            Assert.IsInstanceOf(typeof(String), sayOutPut);
+        }
+    }
+}
