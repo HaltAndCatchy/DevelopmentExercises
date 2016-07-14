2eb748669070ac2944145b125cda5d4f0819db32
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-14 15:55:12 +0100

    Messing up commits again.

---
+++ DevelopmentExercises/Counter.Tests/CSharpDevving/Exercises_CSharp_July/YellowConsoleApp/GlazerCalc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowBook
+{
+    public class GlazerCalc
+    {
+       public static void Main()
+       {
+           double width, height, woodLength, glassArea;
+           string widthString, heightString;
+
+           widthString = Console.ReadLine();
+           width = double.Parse(widthString);
+
+           heightString = Console.ReadLine();
+           height = double.Parse(heightString);
+
+           woodLength = 2*(width + height)*3.25;
+
+           glassArea = 2*(width*height);
+
+           Console.WriteLine("The length of wood is " +
+               woodLength + "feet");
+           Console.WriteLine("The area of the glass is " + 
+               glassArea + "square metres");
+       }
+    }
+}
