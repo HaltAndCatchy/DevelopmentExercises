ea1a9ce581cbd87ccd29686d78db99c61ccdc82d
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-15 10:58:08 +0100

    Add console message.

--- YellowBook/ProgrammingFundamentals/GlazerCalc.cs
+++ YellowBook/ProgrammingFundamentals/GlazerCalc.cs
@@ -13,24 +13,23 @@ namespace YellowBook
            Console.Write("Give the width of the window : ");
 
 
+
            double height = Height();
            double width = Width();
            
-           const double MAX_WIDTH = 5.0;
-           const double MIN_WIDTH = 0.5;
-           const double MAX_HEIGHT = 3.0;
-           const double MIN_HEIGHT = 0.75;
+           //const double MAX_WIDTH = 5.0;
+           //const double MIN_WIDTH = 0.5;
+           //const double MAX_HEIGHT = 3.0;
+           //const double MIN_HEIGHT = 0.75;
            
            double woodLength = 2 * (width + height) * 3.25;
            double glassArea = 2 * (width * height);
 
-
-           
-           
            Console.WriteLine ( "The length of the wood is " + woodLength + " feet" ) ;
            Console.WriteLine( "The area of the glass is " + glassArea + " square metres" ) ;
-           
-          
+           Console.ReadLine();
+
+
        }
 
         public static double Height()
