23106cd3f6ffbfa4f874d63d023de027da88685e
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-18 12:18:03 +0100

    Refactor.

--- YellowBook/ProgrammingFundamentals/Glazer.cs
+++ YellowBook/ProgrammingFundamentals/Glazer.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using ProgrammingFundamentals;
 
 namespace YellowBook
 {
     public class Glazer
     {
-        public  void WindowWidth()
+        public void WindowWidth()
         {
             double width, height, woodLength, glassArea;
 
@@ -14,10 +15,24 @@ namespace YellowBook
             const double MAX_HEIGHT = 3.0;
             const double MIN_HEIGHT = 0.75;
 
-            string  heightString;
+            string heightString;
 
             width = Width(MIN_WIDTH, MAX_WIDTH);
 
+            height = Height(MIN_HEIGHT, MAX_HEIGHT);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width + height);
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+            Console.ReadLine();
+        }
+
+        public double Height(double MIN_HEIGHT, double MAX_HEIGHT)
+        {
+            string heightString;
+            double height;
             do
             {
                 Console.Write("Give the height of the window between " +
@@ -25,13 +40,7 @@ namespace YellowBook
                 heightString = Console.ReadLine();
                 height = double.Parse(heightString);
             } while (height < MIN_HEIGHT || height > MAX_HEIGHT);
-
-            woodLength = 2*(width + height)*3.25;
-            glassArea = 2*(width + height);
-
-            Console.WriteLine("The length of the wood is " + woodLength + " feet");
-            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
-            Console.ReadLine();
+            return height;
         }
 
         public double Width(double MIN_WIDTH, double MAX_WIDTH)
@@ -46,6 +55,8 @@ namespace YellowBook
                 width = double.Parse(widthString);
             } while (width < MIN_WIDTH || width > MAX_WIDTH);
             return width;
+            
         }
+
     }
 }
--- YellowBook/ProgrammingFundamentals/ValueReader.cs
+++ YellowBook/ProgrammingFundamentals/ValueReader.cs
@@ -2,14 +2,14 @@
 
 namespace ProgrammingFundamentals
 {
-    class UsefulMethodExercise
+    public class ValueReader
     {
         public double ReadValue(string prompt,double low,double high )
         {
             double result = 0;
             do
             {
-                Console.WriteLine(prompt + "between" + low + "and" + high);
+                Console.WriteLine(prompt + "between " + low + " and " + high);
                 string resultString = Console.ReadLine();
                 result = double.Parse(resultString);
             } while ((result > low) || (result > high));
