c62fdcfc86a9ec46ade6be6450a0c22dbeb468e9
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-19 15:52:46 +0100

    New class.

---
+++ YellowBook/ProgrammingFundamentals/ArrayDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProgrammingFundamentals
+{
+    class ArrayDemo
+    {
+        public void DemoSomeArrays()
+        {
+            var methodLibrary = new MethodLibrary();
+
+            int[] scores = new int[11];
+            for (int i = 0; i < 11; i = i + 1)
+            {
+                scores[i] = methodLibrary.ReadInt("Score : ", 0, 100);
+            }
+        }
+    }
+}
