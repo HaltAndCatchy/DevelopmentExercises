4224bb5071c513babb479afea3fdb2a1cd90dde5
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-19 15:21:34 +0100

    Add new class and methods.

--- YellowBook/ProgrammingFundamentals/ScopeExamples.cs
+++ YellowBook/ProgrammingFundamentals/ScopeExamples.cs
@@ -18,4 +18,22 @@ namespace ProgrammingFundamentals
             }
         }
     }
+
+    public class MemberExample
+    {
+        public int member = 0;
+
+        public void OtherMethod()
+        {
+            member = 99;
+        }
+
+        public void YetAnotherMethod()
+        {
+            Console.WriteLine("member is : " + member);
+            OtherMethod();
+            Console.WriteLine("member is now: " + member);
+            Console.ReadLine();
+        }
+    }
 }
