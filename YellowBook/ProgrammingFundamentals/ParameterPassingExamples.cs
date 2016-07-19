71f90321fd0bd09a40c0fcd26f0f9b2c143bc959
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-19 14:44:45 +0100

    Add ReadInt method.

--- YellowBook/ProgrammingFundamentals/ParameterPassingExamples.cs
+++ YellowBook/ProgrammingFundamentals/ParameterPassingExamples.cs
@@ -19,9 +19,15 @@ namespace ProgrammingFundamentals
 
         }
 
-        public int readInt(string enterYourAge, int i, int i1)
+        public int readInt(string prompt, int low, int high)
         {
-            throw new NotImplementedException();
+            int result;
+            do
+            {
+                string intString = ReadString(prompt);
+                result = int.Parse(intString);
+            } while ((result < low) || (result > high));
+            return result;
         }
 
         public string ReadString(string prompt)
