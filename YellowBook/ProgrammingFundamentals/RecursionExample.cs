06a774d3d811a9a3bcf1048cb0bff237bd619a9a
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-19 16:38:52 +0100

    Add Recursion example class.

---
+++ YellowBook/ProgrammingFundamentals/RecursionExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProgrammingFundamentals
+{
+    class RecursionExample
+    {
+        public int Recursive(int value, ref int count)
+        {
+            count++;
+            if (value >= 10)
+            {
+                return value;
+            }
+            return Recursive(value + 1, ref count);
+        }
+    }
+}
