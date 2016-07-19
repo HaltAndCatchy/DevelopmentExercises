dd0a4bbb5a20d1c2a03a8d4251e97b5d8390e96a
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-19 10:45:15 +0100

    Add functionality to method.

--- YellowBook/ProgrammingFundamentals/Booleans.cs
+++ YellowBook/ProgrammingFundamentals/Booleans.cs
@@ -13,7 +13,29 @@ namespace ProgrammingFundamentals
                 myBoolA && myBoolB);
             Console.WriteLine("Is either one of them true? {0}",
                 myBoolA || myBoolB);
+        }
 
+        public void ShortCircuiting()
+        {
+            bool boolA;
+
+            Console.WriteLine("| operator: ");
+            boolA = BooleanTrue() | BooleanFalse();
+
+            Console.WriteLine("|| operator: ");
+            boolA = BooleanTrue() || BooleanFalse();
+        }
+
+        private bool BooleanFalse()
+        {
+           Console.WriteLine("Executing BooleanFalse() = False");
+            return false;
+        }
+
+        private bool BooleanTrue()
+        {
+            Console.WriteLine("  Executing BooleanTrue() = True");
+            return true;
         }
     }
 }
