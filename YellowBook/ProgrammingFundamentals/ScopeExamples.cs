04b2b1cdf726ce391edbd8b038d6afa57c3c8662
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-19 15:01:30 +0100

    Add new class and method.

---
+++ YellowBook/ProgrammingFundamentals/ScopeExamples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ProgrammingFundamentals
+{
+    class ScopeExamples
+    {
+        public void UsingScope()
+        {
+            int i;
+            {
+                int j;
+            }
+        }
+    }
+}
