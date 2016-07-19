ee27bb85ff487e9ed9c7902ac935ab27d55e3b40
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-19 16:41:47 +0100

    Add new method to Recursion class.

--- YellowBook/ProgrammingFundamentals/RecursionExample.cs
+++ YellowBook/ProgrammingFundamentals/RecursionExample.cs
@@ -13,5 +13,15 @@ namespace ProgrammingFundamentals
             }
             return Recursive(value + 1, ref count);
         }
+
+        public void Caller()
+        {
+            int count = 0;
+            int total = Recursive(5, ref count);
+
+            Console.WriteLine(total);
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
     }
 }
