4b21406cb44589c479f5d978f4bc2fa5ca71dfae
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-19 10:16:20 +0100

    Add method.

--- YellowBook/ProgrammingFundamentals/Booleans.cs
+++ YellowBook/ProgrammingFundamentals/Booleans.cs
@@ -8,5 +8,16 @@ namespace ProgrammingFundamentals
 {
     class Booleans
     {
+        public void SomeBools()
+        {
+            bool myBoolA = true;
+            bool myBoolB = false;
+
+            Console.WriteLine("Are both Booleans true? {0}", 
+                myBoolA && myBoolB);
+            Console.WriteLine("Is either one of them true? {0}",
+                myBoolA || myBoolB);
+
+        }
     }
 }
