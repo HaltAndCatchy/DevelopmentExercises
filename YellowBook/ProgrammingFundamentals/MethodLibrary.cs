3d06d0d0eb0aae279af2632e4a3c22e280e5a939
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-19 15:53:10 +0100

    New class.

---
+++ YellowBook/ProgrammingFundamentals/MethodLibrary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProgrammingFundamentals
+{
+    public class MethodLibrary
+    {
+        public int ReadInt(string prompt, int low, int high)
+        {
+            int result;
+            do
+            {
+                string intString = ReadString(prompt);
+                result = int.Parse(intString);
+            } while ((result < low) || (result > high));
+            return result;
+        }
+
+        public string ReadString(string prompt)
+        {
+            string result;
+            do{
+                Console.Write(prompt);
+                result = Console.ReadLine();
+            } while (result == "");
+            return result;
+        }
+    }
+}
