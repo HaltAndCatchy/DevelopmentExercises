df58691bd899b9e0d24c974b7562729891d6d409
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-20 14:20:51 +0100

    Add new class.

---
+++ YellowBook/ProgrammingFundamentals/SomeExceptionalCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingFundamentals
+{
+    class SomeExceptionalCode
+    {
+        public int ExceptionalExamples()
+        {
+            int age =111;
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                {
+                }
+                throw;
+            }
+            return age;
+        }
+    }
+}
