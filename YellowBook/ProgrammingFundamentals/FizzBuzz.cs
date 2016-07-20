fd3357fa5e57d0a43d36b49410535e01e9b4c373
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-20 09:45:03 +0100

    Add FizzBuzz classes and methods.

---
+++ YellowBook/ProgrammingFundamentals/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingFundamentals
+{
+    class FizzBuzz
+    {
+        public string Say(ref int fizzBuzzNumber)
+        {
+            string response = "";
+            return response;
+        }
+    }
+
+    internal class UsefulMethods
+    {
+
+        public int ReadInt(string prompt, int low, int high)
+        {
+            int result;
+            do
+            {
+                string intString = ReadString(prompt);
+                result = int.Parse(intString);
+            } while ((result < low) || (result > high));
+            return result;
+        }
+
+        public string ReadString(string prompt)
+        {
+            string result;
+            do
+            {
+                Console.Write(prompt);
+                result = Console.ReadLine();
+            } while (result == "");
+            return result;
+        }
+    }
+
+}
