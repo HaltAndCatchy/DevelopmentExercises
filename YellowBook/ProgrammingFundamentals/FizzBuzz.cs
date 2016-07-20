d92586c840461f5ed363c90933bc3d5ee5dace1d
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-20 09:52:01 +0100

    Change method functionality.

--- YellowBook/ProgrammingFundamentals/FizzBuzz.cs
+++ YellowBook/ProgrammingFundamentals/FizzBuzz.cs
@@ -18,15 +18,11 @@ namespace ProgrammingFundamentals
     internal class UsefulMethods
     {
 
-        public int ReadInt(string prompt, int low, int high)
+        public int ReadInt(string prompt)
         {
-            int result;
-            do
-            {
-                string intString = ReadString(prompt);
-                result = int.Parse(intString);
-            } while ((result < low) || (result > high));
-            return result;
+            string intString = ReadString(prompt);
+            int fizzBuzzNumber = int.Parse(intString);
+            return fizzBuzzNumber;
         }
 
         public string ReadString(string prompt)
