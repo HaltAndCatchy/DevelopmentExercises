551fdfa25bdd8c603815d4276b5400532ddce61d
Jenn Catterall <jenn.catterall@7digital.com>
2016-07-21 14:45:38 +0100

    Add new classes.

---
+++ YellowBook/ProgrammingFundamentals/SwitchFuzzBizz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingFundamentals
+{
+    class SwitchFuzzBizz
+    {
+        public void FuzzBizz()
+        {
+            int userInt = 1;
+            switch (userInt)
+            {
+                case 1 :
+                    HandleModuloThree();
+                    break;
+            }
+        }
+
+        public object HandleModuloThree()
+        {
+            //return handleModuloThree;
+        }
+    }
+}
